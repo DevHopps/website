b75b49a2f21cf35c1283015aa663775f6be444b5
Benedict Balzer <mail@hopps.me>
2017-08-03 08:08:28 +0200

    Create main.js

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//pwk.hopps.me/";
+    _paq.push(['setTrackerUrl', u+'/js']);
+    _paq.push(['setSiteId', '2']);
+    _paq.push(['setAPIUrl', u]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'/js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
