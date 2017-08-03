a33016e65d259800576f57f2cf1510da06d13cb5
Benedict Balzer <mail@hopps.me>
2017-08-03 08:11:22 +0200

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,3 @@
-
-<!-- Piwik -->
-<script type="text/javascript">
   var _paq = _paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
@@ -13,5 +10,3 @@
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'/js'; s.parentNode.insertBefore(g,s);
   })();
-</script>
-<!-- End Piwik Code -->
